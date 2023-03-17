9ecacc6dfdbc043146f4abe3502233c4eb7afcbe
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-17 15:49:23 +0100

    put it in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Description:prints the lowercase alphabet in reverse, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
