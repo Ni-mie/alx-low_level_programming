6b57264673c7004b72aa8c6d1dcb9cbfda7acff9
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-17 14:48:03 +0100

    firsttry

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,7 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * Description: print whether the number stored in the variable n is positive
-   or negative.
+ * Description: print whether the variable n is positive or negative.
  * Return: Always 0 (Success)
  */
 int main(void)
