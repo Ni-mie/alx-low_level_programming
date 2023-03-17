cc7d3c1d0bff8abb5e5acea1125afab7c91aa82f
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-17 15:45:46 +0100

    with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:  prints all single digit numbers of base 10 starting from 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
