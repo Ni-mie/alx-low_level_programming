d186332432e7e494184da5545ad697f0dab3758a
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-17 15:25:27 +0100

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase, and then in uppercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		++CH;
+	}
+	putchar('\n');
+	return (0);
+}
