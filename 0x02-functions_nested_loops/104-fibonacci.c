fa800c74f7bcf5af14bf73dea7c32f13c23804d8
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-21 18:55:21 +0100

    first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long a = 1, b = 2, sum;
+
+	printf("%lu, %lu", a, b);
+
+	for (i = 2; i < 98; i++)
+	{
+		sum = a + b;
+		printf(",");
+		printf(" ");
+		 printf("%lu", sum);
+		a = b;
+		b = sum;
+	}
+	printf("\n");
+	return (0);
+}
