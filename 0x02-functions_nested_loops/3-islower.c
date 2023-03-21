7c991ef0584aa0cddc06fe3b69e71f02a665fc49
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-21 14:01:24 +0100

    is lower description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _islower - a function that checks for lowercase character.
+ * _islower - a function that checks for lowercase character.
  * @c: checks input of function
  * Return: Always 0 (Success)
  */
