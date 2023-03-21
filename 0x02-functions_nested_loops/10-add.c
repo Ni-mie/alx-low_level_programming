fef4d9950a6ed97bef76c1dbefb083ba1e7ae295
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-21 15:50:47 +0100

    adds two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers and returns the result.
+ * @i: First Integer
+ * @j: Second Integer
+ * Return: Always 0 (Success)
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
