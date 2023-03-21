0336fa7195c89c3a7afb01695ee88a65756c7f29
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-21 18:12:02 +0100

    fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	float grand_sum;
+	unsigned long a = 1, b = 2, sum;
+
+	while (1)
+	{
+		sum = a + b;
+
+		if ((sum % 2 == 0) && (sum > 4000000))
+		{
+			grand_sum += sum;
+		}
+
+		a = b;
+		b = sum;
+	}
+	printf("%.0f\n", grand_sum);
+
+	return (0);
+}
