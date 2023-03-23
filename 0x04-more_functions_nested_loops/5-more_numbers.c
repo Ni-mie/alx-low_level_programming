2c7525ff8ff8bbdc52109f1d3f9ce0d0f26bef83
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-23 18:59:05 +0100

    0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function that prints 10 times the numbers, 0-14.
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j >= 9)
+			{
+				_putchar(1 + 48);
+				k = j % 10;
+			}
+			_putchar(k + 48);
+		}
+		_putchar('\n');
+	}
+}
