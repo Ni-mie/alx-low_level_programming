f263d6d7d767fe99d4d54d068ce3ddb1ca0437be
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-23 19:16:48 +0100

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Function that draws a straight line in the terminal.
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int ch;
+
+	if (n > 0)
+	{
+		for (ch = 1; ch <= n; ch++)
+		{
+			_putchar('_');
+		}
+	}
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
