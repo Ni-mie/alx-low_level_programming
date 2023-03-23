c35e4ec2177f04712faaac9b178869a5a9123b29
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-23 14:12:22 +0100

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two integers.
+ * @a: integer 1
+ * @b: integer 2
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
