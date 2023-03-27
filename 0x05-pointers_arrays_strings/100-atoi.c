ad9966e212e53c70305bf0772f6748207eb29ee0
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-27 22:16:34 +0100

    convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+/**
+ * _atoi -Function that cnverts a string to an integer
+ * @s: string
+ * Return: converted integer from string
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
