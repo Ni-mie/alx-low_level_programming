19042f6f05226c37b8abb0a935de88f55f8538ef
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-27 20:38:58 +0100

    put it in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* rev_string - Function that reverses a string
+* @s: string
+* Return: String in reverse
+**/
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int count = 0;
+	int n;
+
+	while (s[count] != '\0')
+		count++;
+
+	for (n = 0; n < count; n++)
+	{
+		count--;
+		rev = s[n];
+		s[n] = s[count];
+		s[count] = rev;
+	}
+}
