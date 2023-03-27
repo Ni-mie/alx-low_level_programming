866e716bc53d737f23b033614eecd881f3a7633f
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-27 19:29:25 +0100

    put string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string, followed by a new line, to stdout.
+ * @str: string
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
