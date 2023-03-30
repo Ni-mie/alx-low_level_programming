136dfa2a9c0b4a77d80fbc63613830b38e6c0d20
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-30 19:37:02 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - Function that encodes a string using rot13.
+ * @str: String
+ * Return: Always 0 (Success)
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
