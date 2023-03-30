44d3143869a09c6a8f72752ab6607e9bdbfe653b
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-03-30 18:43:40 +0100

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - Function that capitalizes all words of a string.
+ * @str: String
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (str[i - 1] == ' ' ||
+			    str[i - 1] == '\t' ||
+			    str[i - 1] == '\n' ||
+			    str[i - 1] == ',' ||
+			    str[i- 1] == ';' ||
+			    str[i - 1] == '.' ||
+			    str[i - 1] == '!' ||
+			    str[i - 1] == '?' ||
+			    str[i - 1] == '"' ||
+			    str[i - 1] == '(' ||
+			    str[i - 1] == ')' ||
+			    str[i - 1] == '{' ||
+			    str[i - 1] == '}' ||)
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
