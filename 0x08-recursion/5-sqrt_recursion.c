f6fc88c5634c196d3faaeee301c2c470e9487ddf
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-05 23:34:08 +0100

    squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int real_sqrt(int n, int j);
+/**
+ * _sqrt_recursion - Function that returns the natural square root of a number.
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (real_sqrt(n, 0));
+}
+
+/**
+ * _sqrt - Function that returns the natural square root of a number.
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+int real_sqrt(int n, int j)
+{
+	if (j * j > n)
+		return (-1);
+	if (j * j == n)
+		return (j);
+	return (real_sqrt(n, j + 1));
+}
