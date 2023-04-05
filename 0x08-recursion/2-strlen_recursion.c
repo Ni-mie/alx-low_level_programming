12861bee0801a8472806740af8e0881fda781733
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-05 21:04:47 +0100

    print length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - Function that returns length of string.
+ * @s: String
+ * Return: Always 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
