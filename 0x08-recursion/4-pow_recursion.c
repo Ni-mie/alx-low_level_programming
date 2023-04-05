721a71e7ed63013025d278b2cd0b945d0ed53733
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-05 22:58:27 +0100

    raised to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - Function that returns the value of
+ * x raised to the power of y.
+ * @x: integer
+ * @y: integer
+ * Return: Always 0 (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, y-1));
+}
