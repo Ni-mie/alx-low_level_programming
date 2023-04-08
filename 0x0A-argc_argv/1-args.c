2dec60a5ee4f90e6b7006e4268957307f5423931
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-08 15:39:24 +0100

    print number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
