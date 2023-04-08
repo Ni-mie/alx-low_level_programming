f1dd82140615eadb10a378ca4eec58962b01805d
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-08 17:24:55 +0100

    money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, cents, rem = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		while (cents >= coins[i])
+		{
+			rem++;
+			cents -= coins[i];
+		}
+	}
+	printf("%d\n", rem);
+	return (0);
+}
