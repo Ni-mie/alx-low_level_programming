14d1edb311e1b9a404765971b63a76b9de73dcc9
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-12 03:08:45 +0100

    args to string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * argstostr - Function that concatenates all the arguments of your program.
+ * @ac: Integer
+ * @av: Char
+ * Return: Always 0 (Success)
+ */
+char *argstostr(int ac, char **av)
+{
+	int j, i = 0, total = 0;
+	char *concat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (j = 0; j < ac; j++)
+	{
+		int len = 0;
+		while (av[j][len])
+			len++;
+		total += len + 1;
+	}
+
+	concat = (char *) malloc(sizeof(char) * total);
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (j = 0; j < ac; j++)
+	{
+		int len = 0;
+		while (av[j][len])
+		{
+			concat[i] = av[j][len];
+			len++;
+			i++;
+		}
+		concat[i] = '\n';
+		i++;
+	}
+
+	concat[i] = '\0';
+	return (concat);
+}
