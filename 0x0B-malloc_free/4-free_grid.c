50c12f4204ccbef7645ce690698c38c152648eaf
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-12 02:40:24 +0100

    fixed error

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * free__grid - Function that frees a 2 dimensional grid previously created by
+ * free_grid - Function that frees a 2 dimensional grid previously created by
  *             your alloc_grid function.
  * @grid: Grid
  * @height: Height
  * Return: Always 0 (Success)
  */
-void free__grid(int **grid, int height)
+void free_grid(int **grid, int height)
 {
 	int i;
 
