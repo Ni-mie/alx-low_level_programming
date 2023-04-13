dfd081586bece936c91ecd3d46ff4c18f37f3ddf
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-13 16:10:33 +0100

    concantenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	concat = (char *) malloc(len1 + n + 1);
+	concat = (char *) malloc(sizeof(char) * (len1 + n + 1));
 	if (concat == NULL)
 		return (NULL);
 
