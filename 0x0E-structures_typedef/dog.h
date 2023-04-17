c3d90ce1c2b9bb30f538dd5395b01e9ce08bc926
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-17 22:19:54 +0100

    initialize

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dog - a dog's basic info
  * @name: String
@@ -16,5 +20,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
