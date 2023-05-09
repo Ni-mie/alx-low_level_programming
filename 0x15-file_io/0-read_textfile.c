f75191f68f55e646b685b6e4bcd3164617a767aa
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-05-09 03:01:41 +0100

    Reads a text file and prints to standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Function that reads a text file and print to STDOUT.
+ * @filename: Text file
+ * @letters: Number of letter
+ * Return: The actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, rd, wr;
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	free(buf);
+	close(fd);
+	return (wr);
+}
