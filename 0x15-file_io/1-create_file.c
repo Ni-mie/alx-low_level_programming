f1b3d87f392a8161aaf97eaf5519624cbe6ce0ae
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-05-09 03:21:08 +0100

    fix checker

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
 
 	if (fd == -1 || wr == -1)
